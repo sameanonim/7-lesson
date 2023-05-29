94c214ff3ff013a56418307c6adf1f55b13c63bc
samea <sameanoniM@gmail.com>
2023-05-29 22:04:42 +0300

    я ленюсь писать все изменения, но они есть. Вот честно.

--- project/catalog/urls.py
+++ project/catalog/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
