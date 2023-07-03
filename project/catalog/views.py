37409099fd39f8f732b60a70f1dd23a0f43946e7
samea <sameanoniM@gmail.com>
2023-07-03 07:40:13 +0300

    modified:   project/catalog/templates/home.html

--- project/catalog/views.py
+++ project/catalog/views.py
@@ -50,7 +50,7 @@ class ProductUpdateView(UpdateView):
     success_url = reverse_lazy('home')
 
     def get_success_url(self, *args, **kwargs):
-        return reverse('update_product', args=[self.get_object().pk])
+        return self.success_url
     
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
